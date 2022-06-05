c896a9ee19b940cf5ab9d4381e2aa2cc8f986b05
Ilan Breines <ilanbreines@gmail.com>
2022-06-04 21:08:06 -0700

    oqfnnfwow

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "portfolio.html"),
+      },
+    },
+  },
+});
